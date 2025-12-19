4ed39cd6e44dcc27abf2e3d39f649cb136872071
upm5017 <uday.samkar.m@gmail.com>
2025-12-19 21:11:19 +0530

    Version 6.3: Final

--- UViz_run.py
+++
@@ -1,5 +0,0 @@
-from U_viz import visualize_molecule as UV
-atoms = ['C', 'O', 'H', 'H', 'N']
-coordinates = [[0.0,0.0,0.],[1.21, 0, 0], [-0.55, 0.96, 0], [-0.55, -0.96, 0], [2.0, 0, 0]]
-ax, plt = UV(atoms, coordinates)
-plt.savefig("Test.png",dpi=450)
